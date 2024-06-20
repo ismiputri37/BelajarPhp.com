3f19ece5b8ea15e331ec6fec2ad73cb3ed2e574e
Ismi Putri <ismi.putri397@gmail.com>
2024-06-20 16:11:12 +0800

    first commit

---
+++ latihan.php
@@ -0,0 +1,37 @@
+<?php 
+// $i++;
+// $i =2;
+// $i = $i+1;
+
+// $a = 5;
+// $b = "5";
+// echo "menggunakan ==";
+// if ($a == $b) {
+//     echo "nilai benar";
+// } else {
+//     echo "nilai salah";
+// }
+
+// echo "<hr>";
+// echo "menggunakan ===";
+// if ($a === $b) {
+//     echo "nilai benar";
+// } else {
+//     echo "nilai salah";
+// }
+
+// perulangan for
+// for ($i=0; $i<=10; $i++) {
+//     echo "perulangan ke-$i <br>";
+// }
+
+$i=5;
+while ($i <= 0) {
+    echo "perulangan while ke-$i";
+    $i--;
+}
+
+
+
+
+?>
